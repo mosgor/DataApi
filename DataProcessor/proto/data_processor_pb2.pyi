8b604c2af1885605e4087a55cbfaf1ae0904b28e
Maxim Mikhailov <mos_gor@list.ru>
2024-11-29 20:03:31 +0300

    Initial commit

---
+++ DataProcessor/proto/data_processor_pb2.pyi
@@ -0,0 +1,17 @@
+from google.protobuf import timestamp_pb2 as _timestamp_pb2
+import common_pb2 as _common_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Data(_message.Message):
+    __slots__ = ("source_id", "data_json", "arrival_time")
+    SOURCE_ID_FIELD_NUMBER: _ClassVar[int]
+    DATA_JSON_FIELD_NUMBER: _ClassVar[int]
+    ARRIVAL_TIME_FIELD_NUMBER: _ClassVar[int]
+    source_id: int
+    data_json: str
+    arrival_time: _timestamp_pb2.Timestamp
+    def __init__(self, source_id: _Optional[int] = ..., data_json: _Optional[str] = ..., arrival_time: _Optional[_Union[_timestamp_pb2.Timestamp, _Mapping]] = ...) -> None: ...
